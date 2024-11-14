9be5048a8e492b749c140d2d06f288b37d897983
izanfergi <izanfergi@gmail.com>
2024-11-14 16:55:20 +0100

    He copiado la pagina web original, voy a ver como hago el juego desde ahí

--- codigo/protectbird.js
+++
@@ -1,6 +0,0 @@
-//Queremos hacer un juego parecido al flappybird, vamos a ver como crearlo.
-//Lo primero que tendremos que hacer será poner el fondo y las imagenes.
-//Despues de ello la velocidad del juego.
-//Despues tendremos que poner valores aleatorios a los tubos para que el tamaño sea aleatorio
-//Luego de ello podnriamos algunas cosas para hacerlo un poqutin de mas dificil.
-//Y ahora tocará pensar en como hacer todo.
