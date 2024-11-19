1860645e40a3d69cdfbcccbc63ac16fbcb62c979
izanfergi <izanfergi@gmail.com>
2024-11-19 09:31:39 +0100

    fix: Hemos hecho las constantes de los tubos, ahora haré que salgan de forma aleatoria

--- js/game.js
+++ js/game.js
@@ -1,4 +1,9 @@
-import PajaroImportar from './pajaro.js'
+//Queremos hacer un juego parecido al flappybird, vamos a ver como crearlo.
+//Lo primero que tendremos que hacer será poner el fondo y las imagenes.
+//Despues de ello la velocidad del juego.
+//Despues tendremos que poner valores aleatorios a los tubos para que el tamaño sea aleatorio
+//Luego de ello podnriamos algunas cosas para hacerlo un poqutin de mas dificil.
+//Y ahora tocará pensar en como hacer todo.
 
 //Empezaremos a hacer un par de constantes
 
@@ -12,19 +17,12 @@ const pajaritoimg = new Image();
 pajaritoimg.src = "../img/ProtectBirdJuegoAvatar.png"
 
 
-const pajaro = PajaroImportar(canvas);
+//por lo que he visto tengo que poner la configuracion del avatar, asi que tendremos q poner la fuerza de salto y esas cosas
 
-//Creamos las constantes y variables necesarias para los tubos
-
-const tuboAncho = 30;
-const huecoentreTubos = 300;
-const velocidadTubos = 10;
-
-
-const tuboimg = new Image();
-
-tuboimg.src = "../img/TuboProtectBird.png"
-
+let pajaritoAltura = canvas.height / 2; //Lo he dividido entre 2 para que empiece en la mitad
+let pajaritoVel = 1; //Pa darle la velocidad, empiezo por cero porque haré una funcion donde vaya pillando velocidad
+const jump = -7; //Para darle lo que tiene que saltar. 
+const gravedad = 0.5; //para hacer la gravedad
 
 
 //Vamos a crear las variables para el score.
@@ -33,10 +31,22 @@ let puntuacion = 0;
 let inicioTime = 0;  // Esto va a ser para la puntiacion, ya que creo que lo mas facil es ir pillandolo por segundos. ya que no tengo hecho aun los tubos, y eso va a ser lo mas complicado, asi que voy a hacerlo de esta manera.
 
 
+//vamos a hacer las constantes de los tubos:
+const tuboAbajo = new Image();
+tuboAbajo.src = "../img/TuboProtectBird.png"
+
+const tuboArriba = new Image();
+tuboArriba.src = "../img/TuboFlappyBirdReverso.png"
+
+const anchoTubo = 55;
+const espacioTubo = 200;
+const velocidadTubo = 4;
+const tubos = [];
+
 //ahora vamos a intentar que salga en pantalla el pajaro, para ver si pilla bien la imagen o si tendré que hacer algunos retoques.
 function dibujarPajarito(){
     dibujo.clearRect(0, 0, canvas.width, canvas.height);//Aquí limpiamos todo lo que hay en pantalla por si acaso
-    dibujo.drawImage(pajaritoimg, 30, pajaro.getAltura(), 90, 80);  //Aqui hacemos que se dibuje el pajaro.
+    dibujo.drawImage(pajaritoimg, 30, pajaritoAltura, 90, 80);  //Aqui hacemos que se dibuje el pajaro.
 
 
 
@@ -47,13 +57,33 @@ function dibujarPajarito(){
     dibujo.fillText(`Score: ${puntuacion}`, canvas.width - 15, 30);
     }
 
-
-
-function dibujarTubos(){
-    //Aquí limpiamos todo lo que hay en pantalla por si acaso
-    //dibujo.drawImage(tuboimg, 775, 180, tuboAncho, 105);  //Aqui hacemos que se dibuje el pajaro.
+function saltoPajarito(){
+    pajaritoVel = jump; //Hacemos esto para ahora crear una funcion donde lo que haremos es si tocamos cierta tecla lo que hará sera hacer que la velocidad del pajaro 
+    //sea igual que el salto, practicamente será para que el salto se inicie.
 }
 
+document.addEventListener("keydown", (event) =>{
+    
+    if (event.code == "Space"){//Aqui lo que hacemos es que si se crea el evento del espacio active la funcion del salto.
+        saltoPajarito();
+    };
+
+});
+
+
+function actualizarPajarito(){
+    pajaritoVel += gravedad; //Aqui hacemos que la velocidad del pajaro vaya aumentando por la gravedad
+    pajaritoAltura+= pajaritoVel; // Aquí hacemos que la altura cambie gracias a la velocidad del pajaro
+
+
+    if (pajaritoAltura > canvas.height - 70){ // Aqui lo que hago es comparar la altura del pajaro con la del canvas
+        pajaritoAltura = canvas.height - 75;//Aqui lo igualo para que no baje de la altura del canvas -75
+    }
+
+    if (pajaritoAltura < 0){
+        pajaritoAltura = 0;
+    }
+}
 
 
 function darScore(){
@@ -68,11 +98,10 @@ function darScore(){
 
 
 function iniciarGame(){
-    pajaro.actualizarPajarito();
     dibujarPajarito();//Esto es para iniciar la funcion de dibujar pajaro.
+    actualizarPajarito();
     requestAnimationFrame(iniciarGame); // Esto es para dibujar la animacion en la pantalla, asi que lo que hará será crear la animacion completa del pajaro.
     darScore();
-    //dibujarTubos();
 }
 
 document.addEventListener("keydown", (event) =>{
@@ -85,13 +114,8 @@ document.addEventListener("keydown", (event) =>{
 
 document.addEventListener("keydown", (event) =>{
     
-    if (event.code == "Space"){//Aqui lo que hacemos es que si se crea el evento del espacio active la funcion del salto.
-        pajaro.saltoPajarito();
-    };
     if (event.code == "Space"){
         event.preventDefault();//Esto es para que cuando le demos al espacio no se mueva la pagina web a la hora de jugar.
     };
 
 });
-
-
